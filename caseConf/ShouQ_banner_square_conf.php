b7cec460f6a2fc6b69b7b7c688916f9059967d47
chenyan <32291522+chenyanqa@users.noreply.github.com>
2017-09-26 15:41:00 +0800

    Create ShouQ_banner_square_conf.php

---
+++ caseConf/ShouQ_banner_square_conf.php
@@ -0,0 +1,10 @@
+<?php
+
+class base_conf {
+
+    public function __construct() {
+        $this->cookie = 'MEILISHUO_MM=eef0b92bda3290b44367105d1ac369d3';
+        $this->uid = 38597397;
+        $this->userName = 'fuckie';
+    }
+}
